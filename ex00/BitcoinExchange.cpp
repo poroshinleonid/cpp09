20d446171c522af4cef53dd4846cdb5cab5f7639
Leonid Poroshin <lporoshi@student.42heilbronn.de>
2024-10-07 11:46:58 +0200

    finish ex00

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -35,21 +35,24 @@ BitcoinExchange::BitcoinExchange(const std::string &db_filename) {
 }
 
 bool BitcoinExchange::is_date_valid(const std::string &date) const {
+  std::tm tm = {};
+  std::istringstream ss(date);
+  ss >> std::get_time(&tm, "%Y-%m-%d");
+  if (ss.fail()) {
+      return false;
+  }
+  tm.tm_isdst = -1;
+  std::time_t time = std::mktime(&tm);
+  if (time == -1) {
+      return false;
+  }
   if (date.size() != 10 || date[4] != '-' || date[7] != '-') {
     return false;
   }
-  int y, m, d;
-  try {
-    y = std::stoi(date.substr(0, 4));
-    m = std::stoi(date.substr(5, 2));
-    d = std::stoi(date.substr(8, 2));
-  } catch (std::exception &e) {
-    return false;
-  }
-  if (y < 1 || m < 1 || d < 1) { // REWORK
-    return false;
-  }
-  return true;
+  std::tm* tm_check = std::localtime(&time);
+  return (tm_check->tm_year == tm.tm_year && 
+          tm_check->tm_mon == tm.tm_mon && 
+          tm_check->tm_mday == tm.tm_mday);
 }
 
 void BitcoinExchange::process_req_file(const char *req_filename) const {
@@ -60,22 +63,37 @@ void BitcoinExchange::process_req_file(const char *req_filename) const {
   }
   std::string line;
   std::getline(ifile, line);
+  if (line != "date | value") {
+    std::cout << "Error in the input file format" << std::endl;
+    return;
+  }
   while (std::getline(ifile, line)) {
+    if (line.size() < 14 || line[4] != '-' || line[7] != '-' || line[10] != ' ' || line[11] != '|' || line[12] != ' ') {
+      std::cout << "Error: bad input. => " << line << std::endl;
+      continue;
+    }
+    std::string date = line.substr(0, 10);
+    if (!is_date_valid(date)) {
+      std::cout << "Error: bad date. => " << date << std::endl;
+      continue;
+    }
+    line.erase(0, 13);
     std::stringstream ss(line);
-    std::string date, amount_str;
-    std::string dummy;
-    if (std::getline(ss, date, ' ') && std::getline(ss, dummy, '|') &&
-        std::getline(ss, dummy, ' ') && std::getline(ss, amount_str)) {
-      if (!is_date_valid(date)) {
-        std::cout << "Error: bad input. => " << date << std::endl;
-        continue;
-      }
+    std::string amount_str;
+    if (std::getline(ss, amount_str)) {
       double amount;
+      size_t endpos;
       try {
-        amount = std::stof(amount_str);
-      } catch (...) {
+        amount = std::stof(amount_str, &endpos);
+      } catch (std::invalid_argument) {
         std::cout << "Error: not a number. => " << amount_str << std::endl;
         continue;
+      } catch (std::out_of_range) {
+        std::cout << "Error: too large a number. => " << amount_str << std::endl;
+        continue;
+      }
+      if (endpos != amount_str.size()) {
+        
       }
       if (amount < 0) {
         std::cout << "Error: not a positive number. => " << amount_str
@@ -88,6 +106,9 @@ void BitcoinExchange::process_req_file(const char *req_filename) const {
         continue;
       }
       show_total_cost(date, amount);
+    } else {
+      std::cout << "Error: bad input. => " << line << std::endl;
+      continue;
     }
   }
   ifile.close();
